ae53ca3422bf5be651613f9a2312d223c6d9f7f4
Glenn-David Daniel <D2rd@outlook.com>
2017-06-07 13:52:28 -0700

    Completes core features skeleton and placeholders.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* set queries.js as var 'db'. */
+var db = require('../queries');
+
+/* set routes */
+router.get('/api/bookstore', db.getAllPuppies);
+router.get('/api/bookstore/:id', db.getSingleBook);
+router.post('/api/bookstore', db.createBook);
+router.put('/api/bookstore/:id', db.updateBook);
+router.delete('/api/bookstore/:id', db.removeBook);
+
+
+/* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+*/
 module.exports = router;
---
+++ sql/list-all-books.sql
@@ -0,0 +1,6 @@
+/** List all books in inventory showing Title, author **/
+SELECT title as "Title", author AS "Author"
+
+FROM books
+
+ORDER BY title ASC;
